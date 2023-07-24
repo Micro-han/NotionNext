da1c5347b8cad670691ab69743c982ee916f38e5
Xiaohan Yan <44750451+Micro-han@users.noreply.github.com>
2023-07-24 16:28:12 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,14 +18,14 @@ const BLOG = {
     CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
   
     AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Microhan', // 您的昵称 例如 tangly1024
-    BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
-    LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
-    KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+    BIO: process.env.NEXT_PUBLIC_BIO || 'I Love Study', // 作者简介
+    LINK: process.env.NEXT_PUBLIC_LINK || 'https://micro-han.top', // 网站地址
+    KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '博客', // 网站关键词 英文逗号隔开
     // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-    CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
+    CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'yxhop666@gmail.com', // 邮箱地址 例如mail@tangly1024.com
     CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
     CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
-    CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
+    CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/Micro-han', // 你的github个人主页 例如 https://github.com/tangly1024
     CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || '', // 你的telegram 地址 例如 https://t.me/tangly_1024
     CONTACT_LINKEDIN: process.env.NEXT_PUBLIC_CONTACT_LINKEDIN || '', // 你的linkedIn 首页
     CONTACT_INSTAGRAM: process.env.NEXT_PUBLIC_CONTACT_INSTAGRAM || '', // 您的instagram地址
@@ -238,13 +238,13 @@ const BLOG = {
         'https://cusdis.com/js/cusdis.es.js', // change this if you're using self-hosted version
   
     // gitalk评论插件 更多参考 https://gitalk.github.io/
-    COMMENT_GITALK_REPO: process.env.NEXT_PUBLIC_COMMENT_GITALK_REPO || '', // 你的Github仓库名，例如 'NotionNext'
-    COMMENT_GITALK_OWNER: process.env.NEXT_PUBLIC_COMMENT_GITALK_OWNER || '', // 你的用户名 e.g tangly1024
-    COMMENT_GITALK_ADMIN: process.env.NEXT_PUBLIC_COMMENT_GITALK_ADMIN || '', // 管理员用户名、一般是自己 e.g 'tangly1024'
+    COMMENT_GITALK_REPO: process.env.NEXT_PUBLIC_COMMENT_GITALK_REPO || 'NotionNext', // 你的Github仓库名，例如 'NotionNext'
+    COMMENT_GITALK_OWNER: process.env.NEXT_PUBLIC_COMMENT_GITALK_OWNER || 'Micro-han', // 你的用户名 e.g tangly1024
+    COMMENT_GITALK_ADMIN: process.env.NEXT_PUBLIC_COMMENT_GITALK_ADMIN || 'Micro-han', // 管理员用户名、一般是自己 e.g 'tangly1024'
     COMMENT_GITALK_CLIENT_ID:
-        process.env.NEXT_PUBLIC_COMMENT_GITALK_CLIENT_ID || '', // e.g 20位ID ， 在gitalk后台获取
+        process.env.NEXT_PUBLIC_COMMENT_GITALK_CLIENT_ID || 'c815ba4662451a6ebff3', // e.g 20位ID ， 在gitalk后台获取
     COMMENT_GITALK_CLIENT_SECRET:
-        process.env.NEXT_PUBLIC_COMMENT_GITALK_CLIENT_SECRET || '', // e.g 40位ID， 在gitalk后台获取
+        process.env.NEXT_PUBLIC_COMMENT_GITALK_CLIENT_SECRET || '5cb6f22ca1d2daec592cb2e8930c9ad0ba28ff48', // e.g 40位ID， 在gitalk后台获取
     COMMENT_GITALK_DISTRACTION_FREE_MODE: false, // 类似facebook的无干扰模式
     COMMENT_GITALK_JS_CDN_URL: process.env.NEXT_PUBLIC_COMMENT_GITALK_JS_CDN_URL || 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js', // gitalk客户端 js cdn
     COMMENT_GITALK_CSS_CDN_URL: process.env.NEXT_PUBLIC_COMMENT_GITALK_CSS_CDN_URL || 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.css', // gitalk客户端 css cdn
